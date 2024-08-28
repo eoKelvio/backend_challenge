f81ccec40bf134e6597b59260587c711a7d68cea
Kelvin Silvestre <kelvinsc7@gmail.com>
2024-08-28 19:32:55 -0300

    fix: removing routes
    foram removidas as rotas antigas

--- app/main.py
+++ app/main.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI
-from src.routes import person, card, account, webhook
+from src.routes import webhook
 
 app = FastAPI()
 
-app.include_router(person.router)
-app.include_router(account.router)
-app.include_router(card.router)
 app.include_router(webhook.router)
