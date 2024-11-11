6c64736decd270373d4d31c066f7b72de38622a9
Kelvin Silvestre <kelvinsc7@gmail.com>
2024-11-11 13:49:51 -0300

    :wrench: chore: mock data change

---
+++ apis/mock-data.py
@@ -0,0 +1,91 @@
+import base64
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+from faker import Faker
+from webhook.app.src.schemas import PersonBody, AccountBody, CardBody
+
+# Gerador de ID crescente
+class IDGenerator:
+    def __init__(self, start=1):
+        self.current_id = start
+
+    def get_next_id(self):
+        next_id = self.current_id
+        self.current_id += 1
+        return next_id
+
+fake = Faker('pt_BR')
+person_id = IDGenerator(start=1)
+account_id = IDGenerator(start=1)
+card_id = IDGenerator(start=1)
+address = f"{fake.street_name()}, {fake.building_number()}, {fake.city()}, {fake.state_abbr()}"
+balance = round(fake.random.uniform(1000, 30000), 2)
+avaliable_balance = round(fake.random.uniform(0, balance), 2)
+
+person_data = PersonBody(
+    id=person_id.get_next_id(),  
+    name=fake.name(),
+    email=fake.email(),
+    gender=fake.random_element(elements=('M', 'F')),
+    birth_date=fake.date_of_birth(minimum_age=18, maximum_age=90).isoformat(),
+    address=address,
+    salary=round(fake.random.uniform(2000, 20000), 2),
+    cpf=fake.cpf().replace('.', '').replace('-', '')
+)
+
+account_data = AccountBody(
+    id=account_id.get_next_id(),
+    status_id=fake.random_int(min=0, max=3),
+    due_day=fake.random_int(min=1, max=30),
+    person_id=person_data.id,
+    balance=balance,
+    avaliable_balance=round(fake.random_number(digits=4), 2)
+)
+
+card_data = CardBody(
+    id=card_id.get_next_id(),
+    card_number=fake.credit_card_number(),
+    account_id=account_data.id,
+    status_id=fake.random_int(min=0, max=3),
+    limit=round(fake.random_number(digits=5), 2),
+    expiration_date=fake.credit_card_expire()
+)
+
+def load_public_key(public_key_path):
+    with open(public_key_path, "rb") as key_file:
+        public_key = serialization.load_pem_public_key(key_file.read())
+    return public_key
+
+def split_data(data, chunk_size):
+    return [data[i:i + chunk_size] for i in range(0, len(data), chunk_size)]
+
+def encrypt_body(body, public_key_path, chunk_size=190):
+    public_key = load_public_key(public_key_path)
+    chunks = split_data(body.encode('utf-8'), chunk_size)
+    
+    encrypted_chunks = []
+    for chunk in chunks:
+        encrypted_chunk = public_key.encrypt(
+            chunk,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+        encrypted_chunks.append(encrypted_chunk)
+    
+    encrypted_body = b''.join(encrypted_chunks)
+    return encrypted_body
+
+person_json = person_data.model_dump_json()
+account_json = account_data.model_dump_json()
+card_json = card_data.model_dump_json()
+print(person_json)
+print(account_json)
+print(card_json)
+
+# encrypted_body = encrypt_body(person_json, "../secrets/public_key.pem")
+# encrypted_body_base64 = base64.b64encode(encrypted_body).decode('utf-8')
+# print(encrypted_body_base64)
--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-uvicorn webhook.app.main:app --host 0.0.0.0 --port 9999 --reload &
-uvicorn storage.app.main:app --host 0.0.0.0 --port 9998 --reload &
-# uvicorn streaming.app.config:app --host 0.0.0.0 --port 9997 --reload &
-
-wait -n
